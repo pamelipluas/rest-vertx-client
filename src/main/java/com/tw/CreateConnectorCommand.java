72dd5e5543952f84651682d60eddc5dd943239a1
Pame Plúas <pamepluas@gmail.com>
2019-10-01 17:30:21 -0500

    Makes CreateConnectorCommand as a Verticle

--- src/main/java/com/tw/CreateConnectorCommand.java
+++ src/main/java/com/tw/CreateConnectorCommand.java
@@ -1,28 +1,27 @@
 package com.tw;
 
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import io.vertx.ext.web.codec.BodyCodec;
 
-public class CreateConnectorCommand {
-    private HttpRequest<JsonObject> request;
-    private Connector connector;
-
-    public CreateConnectorCommand(String ip, Integer port, String path, Vertx vertx, Connector connector) {
-        this.request = WebClient.create(vertx)
-                .post(port, ip, path)
+public class CreateConnectorCommand extends AbstractVerticle {
+    @Override
+    public void start () {
+        HttpRequest<JsonObject> request = WebClient.create(vertx)
+                .post(31918, "192.168.64.12", "/connectors")
                 .as(BodyCodec.jsonObject())
                 .putHeader("Accept", "application/json")
                 .putHeader("Content-Type", "application/json")
                 .expect(ResponsePredicate.SC_CREATED);
-        this.connector = connector;
-    }
-
-    public void run () {
-        this.request.sendJson(this.connector, asyncResult -> {
+        Connector connector = new Connector("file-sink-connector3");
+        connector.addConfigItem("connector.class", "org.apache.kafka.connect.file.FileStreamSinkConnector");
+        connector.addConfigItem("tasks.max", "10");
+        connector.addConfigItem("topics", "test-topic");
+        connector.addConfigItem("file", "/home/test.sink.txt");
+        request.sendJson(connector, asyncResult -> {
             if (asyncResult.succeeded()) {
                 System.out.println(asyncResult.result().body());
                 System.out.println();
--- src/main/java/com/tw/Deployer.java
+++ src/main/java/com/tw/Deployer.java
@@ -5,6 +5,12 @@ import io.vertx.core.AbstractVerticle;
 public class Deployer extends AbstractVerticle {
     @Override
     public void start() throws Exception {
+        Connector connector = new Connector("file-sink-connector3");
+        connector.addConfigItem("connector.class", "org.apache.kafka.connect.file.FileStreamSinkConnector");
+        connector.addConfigItem("tasks.max", "10");
+        connector.addConfigItem("topics", "test-topic");
+        connector.addConfigItem("file", "/home/test.sink.txt");
         vertx.deployVerticle("com.tw.EmptyVerticle");
+        vertx.deployVerticle(new CreateConnectorCommand());
     }
 }
